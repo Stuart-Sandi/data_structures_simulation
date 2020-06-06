45fe0a087b5637178edcb13a0c1420e84456c602
Brayan Marín Quirós <brayanmq11288@gmail.com>
2020-06-06 17:55:39 -0600

    Prueba leer nombres en directorio "armazon"

--- Armazon/main.cpp
+++ Armazon/main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
-
+#include <QDir>
+#include <QDebug>
 #include <QApplication>
+#include <QFileDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    QDir d = QFileInfo("./Armazon").absoluteDir();
+    QString absolute=d.absolutePath();
+
+    QStringList lista = QDir(absolute).entryList();
+    qDebug() << lista;
+
     return a.exec();
 }
