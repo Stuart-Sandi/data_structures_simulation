520d52c36f081b76aa11dee6b8f6239da4ee832f
Brayan Marín Quirós <brayanmq11288@gmail.com>
2020-06-06 18:44:58 -0600

    Se agregó ArticuloAlmacen

---
+++ Armazon/articuloalmacen.cpp
@@ -0,0 +1,6 @@
+#include "articuloalmacen.h"
+
+ArticuloAlmacen::ArticuloAlmacen()
+{
+
+}
---
+++ Armazon/articuloalmacen.h
@@ -0,0 +1,18 @@
+#ifndef ARTICULOALMACEN_H
+#define ARTICULOALMACEN_H
+
+#include <QString>
+
+struct ArticuloAlmacen{
+
+    QString codigo;
+    int cantidad;
+    int segundosFabricacion;
+    QString categoria;
+    QString ubicacionBodega;
+
+
+
+};
+
+#endif // ARTICULOALMACEN_H
